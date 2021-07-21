cce98c6284f3b9dae90f7429d7f31daa7a35fc82
Fonti Kar <f.kar@unsw.edu.au>
2021-07-21 13:00:29 +1000

    Simplified test file and finished off main code

--- R/test-say.R
+++ R/test-say.R
@@ -1,10 +1,3 @@
-test_that("Input is in correct format", {
-  what <- c("This is a character string")
-  say(what)
-  expect_type(what, "character")
-  expect_gt(length(what), 0)
-})
-
 test_that("Output is of correct format",{
   expect_length(say(), 0) #Returns nada
   expect_null(say()) #Returns nada
