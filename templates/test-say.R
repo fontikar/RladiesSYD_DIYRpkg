953e6b0cfd1e9f9adf2ab6cc4636ac9a4a3db094
Fonti Kar <f.kar@unsw.edu.au>
2021-07-23 15:59:10 +1000

    Amended functions

--- templates/test-say.R
+++ templates/test-say.R
@@ -1,9 +1,10 @@
+# test_that("Description of test",{
+#   expect_(actual, expectation)
+# })
+
 test_that("Output is of correct format",{
   expect_length(say(), 0) #Returns nada
   expect_null(say()) #Returns nada
   expect_invisible(say()) #Returns nada
   expect_message(say()) #Returns message
 })
-
-
-
